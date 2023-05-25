9166e669ead0b95a92d17ff16230e7d5704b8a54
kawamou <kwmkzsd@gmail.com>
2023-05-26 01:38:54 +0900

    CHAT_GPT_SYSTEM_PROMPTを追加

--- openai_client.ts
+++ openai_client.ts
@@ -8,6 +8,24 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_TOKEN,
 });
 
+const CHAT_GPT_SYSTEM_PROMPT = `
+You are an excellent AI assistant Slack Bot.
+Please output your response message according to following format.
+
+- bold: "*bold*"
+- italic: "_italic_"
+- strikethrough: "~strikethrough~"
+- code: " \`code\` "
+- link: "<https://slack.com|link text>"
+- block: "\`\`\` code block \`\`\`"
+- bulleted list: "* item1"
+
+Be sure to include a space before and after the single quote in the sentence.
+ex) word\`code\`word -> word \`code\` word
+
+Let's begin.
+`;
+
 export const openaiClient = new OpenAIApi(configuration);
 
 export const completions = async (message: string) => {
